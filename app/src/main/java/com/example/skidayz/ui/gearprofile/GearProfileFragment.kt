d84c2674e32e61962c96b26cd33a3db5cab144b3
aueti <anna.ueti@wsu.edu>
2023-04-29 13:25:10 -0700

    Current progress

--- app/src/main/java/com/example/skidayz/ui/gearprofile/GearProfileFragment.kt
+++ app/src/main/java/com/example/skidayz/ui/gearprofile/GearProfileFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.skidayz.databinding.FragmentGearProfileBinding
+import com.example.skidayz.databinding.FragmentGearBinding
 
 class GearProfileFragment : Fragment() {
 
-    private var _binding: FragmentGearProfileBinding? = null
+    private var _binding: FragmentGearBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class GearProfileFragment : Fragment() {
         val userProfileViewModel =
             ViewModelProvider(this).get(GearProfileViewModel::class.java)
 
-        _binding = FragmentGearProfileBinding.inflate(inflater, container, false)
+        _binding = FragmentGearBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.imageTitle
